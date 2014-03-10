958bd422f9f5881e194a45af56abd6e24e453875
Alexander Hof <olek@cosmofox.net>
2014-03-10 10:54:45 +0100

    added make.conf to make it actually useable now

--- nyx/PORTAGE/land.sh
+++ nyx/PORTAGE/land.sh
@@ -3,14 +3,21 @@
 ################################
 # Files that will be overridden:
 #	- bashrc
+#	- make.conf
 #	- patches/...
 #	- savedconfig/...
 #	- sets/...
-#
+# 
+# The script will create a backup-folder 'NYXBACKUP' of the originals.
 # It is due to the design of portage that I cannot put those files in distinct
 # folders to prevent overriding.
 #################################
 
+mkdir /etc/portage/NYXBACKUP
+cp -r /etc/portage/{bashrc,make.conf,patches,savedconfig,sets} /etc/portage/NYXBACKUP
+
 rm -r /etc/portage/package.*/nyx/*
 
 rsync -aPh portage/ /etc/portage/
+
+mirrorselect -s5
